089bc6df7213f7862c4dc65800b23fda2e8a9863
artemka134 <artem.andreanov28@gmail.com>
2024-11-04 20:23:22 +0300

    релиз

--- Assets/YandexGame/WorkingData/SavesYG.cs
+++ Assets/YandexGame/WorkingData/SavesYG.cs
@@ -10,27 +10,27 @@ namespace YG
         public string language = "ru";
         public bool promptDone;
 
-        public float money = 9999999;
-        public float price_inprovement_quanity_tree = 10;
-        public float price_inprovement_speed_tree = 10;
+        public float money = 0;
+        public float price_inprovement_quanity_tree = 5;
+        public float price_inprovement_speed_tree = 20;
         public float quantity_tree = 0;
         public float amount_of_mined_tree = 1;
         public float required_time_tree = 10f;
 
-        public float price_inprovement_quanity_board = 20;
-        public float price_inprovement_speed_board = 20;
-        public float quantity_board = 100;
+        public float price_inprovement_quanity_board = 600;
+        public float price_inprovement_speed_board = 1500;
+        public float quantity_board = 0;
         public float amount_of_mined_board = 1;
-        public float amount_of_consumed_board = 6;
-        public float required_time_board = 40f;
+        public float amount_of_consumed_board = 20;
+        public float required_time_board = 45f;
         public bool start_board = false;
 
-        public float price_inprovement_quanity_furniture = 50;
-        public float price_inprovement_speed_furniture = 50;
+        public float price_inprovement_quanity_furniture = 40000;
+        public float price_inprovement_speed_furniture = 70000;
         public float quantity_furniture = 0;
         public float amount_of_mined_furniture = 1;
-        public float amount_of_consumed_furniture = 10;
-        public float required_time_furniture = 60f;
+        public float amount_of_consumed_furniture = 100;
+        public float required_time_furniture = 180f;
         public bool start_furniture = false;
         
         public SavesYG()
--- Assets/resources.cs
+++ Assets/resources.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Data.SqlTypes;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.UI;
 using YG;
@@ -16,7 +15,7 @@ public class resources : MonoBehaviour
     public TextMeshProUGUI anim_money_text;
     
     // создание объектов класса resources
-    public static resources_class tree = new resources_class(2, false, 5, true);
+    public static resources_class tree = new resources_class(0.5f, false, 5, true, "<sprite=0>");
     public Image indicator_tree_img;
     public TextMeshProUGUI text_quanity_tree;
     public TextMeshProUGUI amount_of_mined_tree_text;
@@ -25,11 +24,11 @@ public class resources : MonoBehaviour
     public TextMeshProUGUI info_inprovoment_quanity_text_tree;
     public TextMeshProUGUI text_sale_tree;
     public Animator anim_tree;
-    public TMP_InputField enter_qualityl_tree;
+    public InputField enter_qualityl_tree;
     public GameObject Improvet_quantity_button_tree; public GameObject Improvet_speed_button_tree;
 
 
-    public static resources_class board = new resources_class(6, true, 5, false, 1000);
+    public static resources_class board = new resources_class(40, true, 4, false, "<sprite=1>", "<sprite=0>", 100);
     public Image indicator_board_img;
     public TextMeshProUGUI text_quanity_board;
     public TextMeshProUGUI amount_of_mined_board_text;
@@ -39,14 +38,15 @@ public class resources : MonoBehaviour
     public TextMeshProUGUI info_inprovoment_quanity_text_board;
     public TextMeshProUGUI text_sale_board;
     public Animator anim_board;
-    public TMP_InputField enter_qualityl_board;
+    public InputField enter_qualityl_board;
     public GameObject Improvet_quantity_button_board; public GameObject Improvet_speed_button_board; public GameObject button_start_board;
     public GameObject panel_board;
     public GameObject text_board_obj;
     public GameObject sale_panel_board;
+    public GameObject button_sale_board;
 
 
-    public static resources_class furniture = new resources_class(12, true, 5, false, 10000);
+    public static resources_class furniture = new resources_class(5000, true, 2, false, "<sprite=2>", "<sprite=1>", 80000);
     public Image indicator_furniture_img;
     public TextMeshProUGUI text_quanity_furniture;
     public TextMeshProUGUI amount_of_mined_furniture_text;
@@ -56,17 +56,19 @@ public class resources : MonoBehaviour
     public TextMeshProUGUI info_inprovoment_quanity_text_furniture;
     public TextMeshProUGUI text_sale_furniture;
     public Animator anim_furniture;
-    public TMP_InputField enter_qualityl_furniture;
+    public InputField enter_qualityl_furniture;
     public GameObject Improvet_quantity_button_furniture; public GameObject Improvet_speed_button_furniture; public GameObject button_start_furniture;
     public GameObject panel_furniture;
     public GameObject text_furniture_obj;
     public GameObject sale_panel_furniture;
+    public GameObject buttton_sale_furniture;
 
 
     public static resources_class[] obj = new resources_class[] {tree, board, furniture}; 
     void Start()
     {
-        YandexGame.ResetSaveProgress();
+        //YandexGame.ResetSaveProgress();
+
         tree.price_inprovement_quanity = YandexGame.savesData.price_inprovement_quanity_tree;
         tree.price_inprovement_speed = YandexGame.savesData.price_inprovement_speed_tree;
         tree.quantity_resources = YandexGame.savesData.quantity_tree;
@@ -126,7 +128,9 @@ public class resources : MonoBehaviour
         if (YandexGame.savesData.start_board == true)
 		{
             panel_board.SetActive(true);
-            sale_panel_board.SetActive(false);
+            sale_panel_board.SetActive(true);
+            button_sale_board.SetActive(false);
+            text_board_obj.SetActive(true);
 		}
 
 
@@ -149,7 +153,9 @@ public class resources : MonoBehaviour
         if (YandexGame.savesData.start_furniture == true)
         {
             panel_furniture.SetActive(true);
-            sale_panel_furniture.SetActive(false);
+            sale_panel_furniture.SetActive(true);
+            buttton_sale_furniture.SetActive(false);
+            text_furniture_obj.SetActive(true);
         }
 
         tree.anim_money = anim_money;
@@ -159,13 +165,14 @@ public class resources : MonoBehaviour
         furniture.anim_money_text = anim_money_text;
         furniture.anim_money = anim_money;
         tree.check_text();
+        tree.check_textur_button();
     }
     void Update()
     {
         tree.calculation_time(ref tree.quantity_resources);
         board.calculation_time(ref tree.quantity_resources);
         furniture.calculation_time(ref board.quantity_resources);
-        money_text.text = YandexGame.savesData.money.ToString("0.0");
+        money_text.text = YandexGame.savesData.money.ToString("0.0") + " " + "<sprite=3>";
 
 
         if (YandexGame.savesData.money >= board.price_start_resources)
@@ -248,13 +255,15 @@ public class resources : MonoBehaviour
     public class resources_class
     {
         public bool start_resources;
-        public resources_class(float price_resources_in, bool other_resources_in, float acceler_rate, bool start_res, float price_start = 0)
+        public resources_class(float price_resources_in, bool other_resources_in, float acceler_rate, bool start_res, string e, string e1 = "", float price_start = 0)
         {
             price_resources = price_resources_in;
             other_resources = other_resources_in;
             acceleration_rate = acceler_rate;
             start_resources = start_res;
             price_start_resources = price_start;
+            emj = e;
+            emj1 = e1;
         }
         public bool other_resources;
         public bool getting_resource = true;
@@ -269,7 +278,7 @@ public class resources : MonoBehaviour
         public TextMeshProUGUI anim_money_text;
         public Animator anin;
         public Animator anim_money;
-        public TMP_InputField enter_qualityl;
+        public InputField enter_qualityl;
         public GameObject Improvet_quantity_button; public GameObject Improvet_speed_button; public GameObject button_start_resoursec;
         public GameObject panel_resources;
         public GameObject text_resources_obj;
@@ -285,6 +294,8 @@ public class resources : MonoBehaviour
         public float consumed;
         public float acceleration_rate;
         public float price_start_resources;
+        public string emj;
+        public string emj1;
 
         public void calculation_time(ref float used_resources)
         {
@@ -328,17 +339,17 @@ public class resources : MonoBehaviour
         }
         public void check_text_void()
         {
-            text_quanity.text = quantity_resources.ToString("0");
+            text_quanity.text = quantity_resources.ToString("0") + " " + emj; 
             required_time_text.text = required_time.ToString("0.0") + "сек";
-            amount_of_mined_resources_text.text = "+" + amount_of_mined_resources.ToString("0");
+            amount_of_mined_resources_text.text = "+" + amount_of_mined_resources.ToString("0") + " " + emj;
             if (other_resources == true)
             {
-                consumed_text.text = "-" + consumed.ToString("0");
+                consumed_text.text = "-" + consumed.ToString("0") + " " + emj1;
             }
-            info_inprovoment_quanity_text.text = "Улучшить производство за " + price_inprovement_quanity.ToString("0.0");
+            info_inprovoment_quanity_text.text = "Улучшить производство за " + price_inprovement_quanity.ToString("0.0") + " " + "<sprite=3>";
             if (required_time > 1)
             {
-                info_inprovoment_speed_text.text = "Улучшить скорость за " + price_inprovement_speed.ToString("0.0");
+                info_inprovoment_speed_text.text = "Улучшить скорость за " + price_inprovement_speed.ToString("0.0") + " " + "<sprite=3>";
             }
             else
             {
@@ -376,28 +387,36 @@ public class resources : MonoBehaviour
             }
 			
 		}
+        // улучшение колличества производства
         public void Improvet_quantity()
         {
             if (YandexGame.savesData.money >= price_inprovement_quanity)
             {
+                anim_money.SetBool("a", true);
+                anim_money.Play("anim_money", 0, 0.0f);
+                anim_money_text.text = "-" + price_inprovement_quanity.ToString("0") + " " + "<sprite=3>";
                 YandexGame.savesData.money -= price_inprovement_quanity;
                 price_inprovement_quanity += price_inprovement_quanity * 0.1f;
                 amount_of_mined_resources += Convert.ToSingle(Math.Ceiling(amount_of_mined_resources * 0.1f));
                 if (other_resources == true)
                 {
-                    consumed += consumed * 0.1f;
+                    consumed += consumed * 0.8f;
                 }
                 Save();
                 check_text();
                 check_textur_button();
             }
         }
+        // улучшение скорости производства
         public void inprover_speed()
         {
             if (YandexGame.savesData.money >= price_inprovement_speed && required_time > 1)
             {
+                anim_money.SetBool("a", true);
+                anim_money.Play("anim_money", 0, 0.0f);
+                anim_money_text.text = "-" + price_inprovement_speed.ToString("0") + " " + "<sprite=3>";
                 YandexGame.savesData.money -= price_inprovement_speed;
-                price_inprovement_speed += price_inprovement_speed * 0.1f;
+                price_inprovement_speed += price_inprovement_speed * 4f;
                 required_time -= Convert.ToSingle(Math.Ceiling(required_time * 0.1f));
                 Save();
                 check_text();
@@ -406,13 +425,18 @@ public class resources : MonoBehaviour
         }
         public void quantity_sold_resources_check()
         {
-            if (enter_qualityl.text != "" && Convert.ToSingle(enter_qualityl.text) > quantity_resources)
+            if (enter_qualityl.text == "")
+            {
+                enter_qualityl.text = "0";
+            }
+            if (Convert.ToSingle(enter_qualityl.text) > quantity_resources)
             {
                 quantity_sold_resources_maximum();
             }
             else
             {
                 quantity_sold_resources = Convert.ToSingle(enter_qualityl.text);
+                enter_qualityl.text = quantity_sold_resources.ToString();
                 check_text_sales();
             }
         }
@@ -428,17 +452,18 @@ public class resources : MonoBehaviour
             {
                 anim_money.SetBool("a", true);
                 anim_money.Play("anim_money", 0, 0.0f);
-                anim_money_text.text = "+" + (quantity_sold_resources * price_resources);
+                anim_money_text.text = "+" + (quantity_sold_resources * price_resources) + " " + "<sprite=3>";
                 YandexGame.savesData.money += quantity_sold_resources * price_resources;
                 quantity_resources -= quantity_sold_resources;
                 quantity_sold_resources_check();
                 check_text();
                 check_textur_button();
+                Save();
             }
         }
         public void check_text_sales()
         {
-            text_sale.text = "Продать за " + (quantity_sold_resources * price_resources);
+            text_sale.text = "Продать за " + (quantity_sold_resources * price_resources) + " " + "<sprite=3>";
         }
         // ускорение производства
         public void boost()
@@ -458,6 +483,7 @@ public class resources : MonoBehaviour
                 panel_resources.SetActive(true);
                 text_resources_obj.SetActive(true);
                 panel_sale.SetActive(true);
+                tree.check_textur_button();
 			}
 		}
     }
